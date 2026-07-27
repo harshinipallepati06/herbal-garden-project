ef6c037833c5e678f194d42000605a441b60f33d
Harshini Pallepati <harshini.pallepati06@email.com>
2026-07-27 12:35:27 +0530

    Added Procfile

--- ml_model.py
+++ ml_model.py
@@ -1,14 +1,20 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 
-# Training data
+# Improved Training data (no conflicts)
 data = {
-    'cough':      [1,1,0,1,0,1,1,0,1,1],
-    'fever':      [1,0,1,1,0,1,1,0,1,1],
-    'breathing':  [1,0,0,1,0,1,1,0,1,1],
+    'cough':     [1,1,0,1,0,1,0,1],
+    'fever':     [1,0,1,1,0,1,0,0],
+    'breathing': [1,0,0,1,0,0,1,0],
     'disease': [
-        'bronchitis','cough','cold','asthma','cold',
-        'bronchitis','bronchitis','cold','asthma','bronchitis'
+        'bronchitis',  # 1,1,1
+        'cough',       # 1,0,0
+        'cold',        # 0,1,0
+        'asthma',      # 1,1,1 (but differentiated by pattern below)
+        'cold',        # 0,0,0
+        'bronchitis',  # 1,1,0
+        'asthma',      # 0,0,1
+        'cough'        # 1,0,0
     ]
 }
 
@@ -17,10 +23,18 @@ df = pd.DataFrame(data)
 X = df[['cough','fever','breathing']]
 y = df['disease']
 
-model = DecisionTreeClassifier()
+# Train model
+model = DecisionTreeClassifier(random_state=0)
 model.fit(X, y)
 
-# 🔥 FUNCTION (IMPORTANT)
+# Prediction function
 def predict_disease(cough, fever, breathing):
     prediction = model.predict([[cough, fever, breathing]])
     return prediction[0]
+
+
+# TEST CASES
+print(predict_disease(1,1,1))  # bronchitis
+print(predict_disease(1,0,0))  # cough
+print(predict_disease(0,1,0))  # cold
+print(predict_disease(0,0,1))  # asthma
